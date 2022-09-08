c04f65e33652589f85b3430320077ecffc593ae0
Javlon Khalimjonov <javlon.khalimjonov@movecloser.pl>
2022-09-08 22:52:06 +0200

    Add semantic ui

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+import SuiVue from 'semantic-ui-vue';
+
+
 Vue.config.productionTip = false
+
+Vue.use(SuiVue);
 
 new Vue({
   store,
